3cdc5c6bdbec419d182fa113a7daca37a7c46111
Dan Feder <dafeder@gmail.com>
2019-07-31 06:51:27 -0500

    Remove Storage interface

---
+++ src/RemoverInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Contracts;
+
+interface RemoverInterface
+{
+  /**
+   * Remove.
+   *
+   * @param string $id
+   *   The identifier for the data.
+   */
+  public function remove(string $id);
+}
