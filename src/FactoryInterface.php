65c65728908c6e2c96562947f9001a52e12f26a2
fmizzell <fmizzell.dev@gmail.com>
2019-08-12 14:37:31 -0500

    Document the factory interface.

--- src/FactoryInterface.php
+++ src/FactoryInterface.php
@@ -4,5 +4,16 @@ namespace Contracts;
 
 interface FactoryInterface
 {
-    public function getInstance($identifier);
+  /**
+   * Construct or deliver an object of the expected class.
+   *
+   * For example a MemoryStorage factory should return
+   * MemoryStorage objects.
+   *
+   * @param string $identifier
+   *   Some way to discern between different instances of a class.
+   *
+   * @return mixed
+   */
+    public function getInstance(string $identifier);
 }
