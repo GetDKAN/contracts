f342f46ad8fdd81697bc3915d1250db2e4ee2271
fmizzell <fmizzell@users.noreply.github.com>
2019-08-13 09:55:30 -0500

    Include a factory interface. (#9)

---
+++ src/FactoryInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Contracts;
+
+interface FactoryInterface
+{
+  /**
+   * Construct or deliver an object of the expected class.
+   *
+   * For example a MemoryStorage factory should return
+   * MemoryStorage objects.
+   *
+   * @param string $identifier
+   *   Some way to discern between different instances of a class.
+   *
+   * @return mixed
+   */
+    public function getInstance(string $identifier);
+}
