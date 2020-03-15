ef8c159f09ac6d5b3adc0fa6a4b01d40fe6a9c58
fmizzell <fmizzell@users.noreply.github.com>
2020-03-15 16:18:22 -0500

    Adds BulkStorerInterface. (#13)

---
+++ src/BulkStorerInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Contracts;
+
+interface BulkStorerInterface
+{
+
+  /**
+   * Store multiple.
+   *
+   * @param array $data
+   *   An array of strings to be stored.
+   */
+    public function storeMultiple(array $data);
+}
