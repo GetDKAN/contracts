95e9c98e57b010516f41db182cd4a509dc639898
nvolodin <nikolay@openconcept.ca>
2021-04-14 18:02:40 -0400

    StorerInterface. Used RootedJsonData

--- src/StorerInterface.php
+++ src/StorerInterface.php
@@ -2,12 +2,14 @@
 
 namespace Contracts;
 
+use RootedData\RootedJsonData;
+
 interface StorerInterface
 {
   /**
    * Store.
    *
-   * @param string|HydratableInterface $data
+   * @param \RootedData\RootedJsonData $data
    *   The data to be stored.
    * @param string $id
    *   The identifier for the data. If the act of storing generates the
@@ -19,5 +21,5 @@ interface StorerInterface
    * @throws \Exception
    *   Issues storing the data.
    */
-    public function store($data, string $id = null): string;
+  public function store(RootedJsonData $data, string $id = null): string;
 }
